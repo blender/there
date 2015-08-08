fefb9532c4644d99b3568617cbc620d82e40ce68
Tommaso Piazza <tommaso.piazza@wearezeta.com>
2015-08-08 14:28:38 +0200

    Initial Commit

---
+++ there/there.h
@@ -0,0 +1,19 @@
+//
+//  there.h
+//  there
+//
+//  Created by Tommaso Piazza on 08/08/15.
+//  Copyright (c) 2015 Alloc Init. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for there.
+FOUNDATION_EXPORT double thereVersionNumber;
+
+//! Project version string for there.
+FOUNDATION_EXPORT const unsigned char thereVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <there/PublicHeader.h>
+
+
